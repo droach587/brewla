dfcfc6c0a1efe1d51fbe039ed480a8c0ac35e7c0
David Roach <Droach587@gmail.com>
2016-03-30 01:09:16 -0400

    Updating
    Signed-off-by: David Roach <Droach587@gmail.com>

---
+++ source/js/main.js
@@ -0,0 +1,77 @@
+var mainJs = (function () {
+ 		
+ 		/**
+ 		 * Avoid Console Errors
+ 		 *
+ 		 * 
+ 		 */
+		(function() {
+		    var method;
+		    var noop = function () {};
+		    var methods = [
+		        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+		        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+		        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+		        'timeStamp', 'trace', 'warn'
+		    ];
+		    var length = methods.length;
+		    var console = (window.console = window.console || {});
+		
+		    while (length--) {
+		        method = methods[length];
+		
+		        // Only stub undefined methods.
+		        if (!console[method]) {
+		            console[method] = noop;
+		        }
+		    }
+		}());
+		
+		function mobileNav(){
+			
+			var $element = $('.mobile-nav-toggle');
+			var active = 'isActive';
+			var $target = $('.primary-nav');
+			
+			$element.on('click', function(e){
+				
+				$target.toggleClass(active);
+				$element.toggleClass(active);
+				
+				e.preventDefault();
+			});
+			
+		}
+		
+		function search(){
+			
+			var $element = $('.primary-nav__search a');
+			var $target = $('.primary-header__search');
+			var active = 'isActive';
+			
+			$element.on('click', function(e){
+				$target.toggleClass(active);
+				e.preventDefault();
+			});
+		}
+ 		
+ 		/**
+ 		 * Simple Init Function
+ 		 *
+ 		 * 
+ 		 */
+        function init() {
+            mobileNav();
+            search();
+        }
+ 
+ 
+        /**
+         * Reveal All Methods here
+         *
+         * 
+         */
+        return {
+            init 	:	init
+        };
+})();
